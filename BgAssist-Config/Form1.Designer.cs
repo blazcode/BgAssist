f98b0aaa9250ed7941ec588ba947b328abe3689a
Corey Blaz <corey@blaz.email>
2020-08-09 17:44:32 -0400

    First commit for BgAssist-Config utility. Laying the foundation for configuring the application via UI.

--- BgAssist-Config/Form1.Designer.cs
+++ BgAssist-Config/Form1.Designer.cs
@@ -28,20 +28,277 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.lblTimer = new System.Windows.Forms.Label();
+            this.numericUpDownTimer = new System.Windows.Forms.NumericUpDown();
+            this.lblPath = new System.Windows.Forms.Label();
+            this.btnSetConfigPath = new System.Windows.Forms.Button();
+            this.txtBgInfoConfigPath = new System.Windows.Forms.TextBox();
+            this.lblBgInfoPath = new System.Windows.Forms.Label();
+            this.txtBgInfoPath = new System.Windows.Forms.TextBox();
+            this.btnSetBgInfoPath = new System.Windows.Forms.Button();
+            this.checkBoxPopup = new System.Windows.Forms.CheckBox();
+            this.checkBoxSilent = new System.Windows.Forms.CheckBox();
+            this.checkBoxTaskbar = new System.Windows.Forms.CheckBox();
+            this.checkBoxAll = new System.Windows.Forms.CheckBox();
+            this.checkBoxLog = new System.Windows.Forms.CheckBox();
+            this.checkBoxRTF = new System.Windows.Forms.CheckBox();
+            this.groupBoxBgInfoOptions = new System.Windows.Forms.GroupBox();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.saveToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownTimer)).BeginInit();
+            this.groupBoxBgInfoOptions.SuspendLayout();
+            this.menuStrip1.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // lblTimer
+            // 
+            this.lblTimer.AutoSize = true;
+            this.lblTimer.Location = new System.Drawing.Point(6, 52);
+            this.lblTimer.Name = "lblTimer";
+            this.lblTimer.Size = new System.Drawing.Size(33, 13);
+            this.lblTimer.TabIndex = 0;
+            this.lblTimer.Text = "Timer";
+            // 
+            // numericUpDownTimer
+            // 
+            this.numericUpDownTimer.AccessibleDescription = "";
+            this.numericUpDownTimer.Location = new System.Drawing.Point(45, 50);
+            this.numericUpDownTimer.Maximum = new decimal(new int[] {
+            300,
+            0,
+            0,
+            0});
+            this.numericUpDownTimer.Name = "numericUpDownTimer";
+            this.numericUpDownTimer.Size = new System.Drawing.Size(55, 20);
+            this.numericUpDownTimer.TabIndex = 1;
+            // 
+            // lblPath
+            // 
+            this.lblPath.AutoSize = true;
+            this.lblPath.Location = new System.Drawing.Point(6, 26);
+            this.lblPath.Name = "lblPath";
+            this.lblPath.Size = new System.Drawing.Size(62, 13);
+            this.lblPath.TabIndex = 2;
+            this.lblPath.Text = "Config Path";
+            // 
+            // btnSetConfigPath
+            // 
+            this.btnSetConfigPath.Location = new System.Drawing.Point(378, 21);
+            this.btnSetConfigPath.Name = "btnSetConfigPath";
+            this.btnSetConfigPath.Size = new System.Drawing.Size(75, 23);
+            this.btnSetConfigPath.TabIndex = 3;
+            this.btnSetConfigPath.Text = "Browse";
+            this.btnSetConfigPath.UseVisualStyleBackColor = true;
+            this.btnSetConfigPath.Click += new System.EventHandler(this.btnSetConfigPath_Click);
+            // 
+            // txtBgInfoConfigPath
+            // 
+            this.txtBgInfoConfigPath.Enabled = false;
+            this.txtBgInfoConfigPath.Location = new System.Drawing.Point(74, 23);
+            this.txtBgInfoConfigPath.Name = "txtBgInfoConfigPath";
+            this.txtBgInfoConfigPath.Size = new System.Drawing.Size(298, 20);
+            this.txtBgInfoConfigPath.TabIndex = 4;
+            // 
+            // lblBgInfoPath
+            // 
+            this.lblBgInfoPath.AutoSize = true;
+            this.lblBgInfoPath.Location = new System.Drawing.Point(12, 38);
+            this.lblBgInfoPath.Name = "lblBgInfoPath";
+            this.lblBgInfoPath.Size = new System.Drawing.Size(63, 13);
+            this.lblBgInfoPath.TabIndex = 5;
+            this.lblBgInfoPath.Text = "BgInfo Path";
+            // 
+            // txtBgInfoPath
+            // 
+            this.txtBgInfoPath.Enabled = false;
+            this.txtBgInfoPath.Location = new System.Drawing.Point(81, 35);
+            this.txtBgInfoPath.Name = "txtBgInfoPath";
+            this.txtBgInfoPath.Size = new System.Drawing.Size(296, 20);
+            this.txtBgInfoPath.TabIndex = 6;
+            // 
+            // btnSetBgInfoPath
+            // 
+            this.btnSetBgInfoPath.Location = new System.Drawing.Point(384, 33);
+            this.btnSetBgInfoPath.Name = "btnSetBgInfoPath";
+            this.btnSetBgInfoPath.Size = new System.Drawing.Size(75, 23);
+            this.btnSetBgInfoPath.TabIndex = 7;
+            this.btnSetBgInfoPath.Text = "Browse";
+            this.btnSetBgInfoPath.UseVisualStyleBackColor = true;
+            this.btnSetBgInfoPath.Click += new System.EventHandler(this.btnSetBgInfoPath_Click);
+            // 
+            // checkBoxPopup
+            // 
+            this.checkBoxPopup.AutoSize = true;
+            this.checkBoxPopup.Location = new System.Drawing.Point(9, 76);
+            this.checkBoxPopup.Name = "checkBoxPopup";
+            this.checkBoxPopup.Size = new System.Drawing.Size(57, 17);
+            this.checkBoxPopup.TabIndex = 9;
+            this.checkBoxPopup.Text = "Popup";
+            this.checkBoxPopup.UseVisualStyleBackColor = true;
+            // 
+            // checkBoxSilent
+            // 
+            this.checkBoxSilent.AutoSize = true;
+            this.checkBoxSilent.Location = new System.Drawing.Point(8, 100);
+            this.checkBoxSilent.Name = "checkBoxSilent";
+            this.checkBoxSilent.Size = new System.Drawing.Size(52, 17);
+            this.checkBoxSilent.TabIndex = 10;
+            this.checkBoxSilent.Text = "Silent";
+            this.checkBoxSilent.UseVisualStyleBackColor = true;
+            // 
+            // checkBoxTaskbar
+            // 
+            this.checkBoxTaskbar.AutoSize = true;
+            this.checkBoxTaskbar.Location = new System.Drawing.Point(8, 124);
+            this.checkBoxTaskbar.Name = "checkBoxTaskbar";
+            this.checkBoxTaskbar.Size = new System.Drawing.Size(65, 17);
+            this.checkBoxTaskbar.TabIndex = 11;
+            this.checkBoxTaskbar.Text = "Taskbar";
+            this.checkBoxTaskbar.UseVisualStyleBackColor = true;
+            // 
+            // checkBoxAll
+            // 
+            this.checkBoxAll.AutoSize = true;
+            this.checkBoxAll.Location = new System.Drawing.Point(9, 148);
+            this.checkBoxAll.Name = "checkBoxAll";
+            this.checkBoxAll.Size = new System.Drawing.Size(37, 17);
+            this.checkBoxAll.TabIndex = 12;
+            this.checkBoxAll.Text = "All";
+            this.checkBoxAll.UseVisualStyleBackColor = true;
+            // 
+            // checkBoxLog
+            // 
+            this.checkBoxLog.AutoSize = true;
+            this.checkBoxLog.Location = new System.Drawing.Point(8, 172);
+            this.checkBoxLog.Name = "checkBoxLog";
+            this.checkBoxLog.Size = new System.Drawing.Size(44, 17);
+            this.checkBoxLog.TabIndex = 13;
+            this.checkBoxLog.Text = "Log";
+            this.checkBoxLog.UseVisualStyleBackColor = true;
+            // 
+            // checkBoxRTF
+            // 
+            this.checkBoxRTF.AutoSize = true;
+            this.checkBoxRTF.Location = new System.Drawing.Point(8, 196);
+            this.checkBoxRTF.Name = "checkBoxRTF";
+            this.checkBoxRTF.Size = new System.Drawing.Size(47, 17);
+            this.checkBoxRTF.TabIndex = 14;
+            this.checkBoxRTF.Text = "RTF";
+            this.checkBoxRTF.UseVisualStyleBackColor = true;
+            // 
+            // groupBoxBgInfoOptions
+            // 
+            this.groupBoxBgInfoOptions.Controls.Add(this.lblPath);
+            this.groupBoxBgInfoOptions.Controls.Add(this.checkBoxRTF);
+            this.groupBoxBgInfoOptions.Controls.Add(this.btnSetConfigPath);
+            this.groupBoxBgInfoOptions.Controls.Add(this.checkBoxLog);
+            this.groupBoxBgInfoOptions.Controls.Add(this.txtBgInfoConfigPath);
+            this.groupBoxBgInfoOptions.Controls.Add(this.checkBoxAll);
+            this.groupBoxBgInfoOptions.Controls.Add(this.lblTimer);
+            this.groupBoxBgInfoOptions.Controls.Add(this.checkBoxTaskbar);
+            this.groupBoxBgInfoOptions.Controls.Add(this.numericUpDownTimer);
+            this.groupBoxBgInfoOptions.Controls.Add(this.checkBoxSilent);
+            this.groupBoxBgInfoOptions.Controls.Add(this.checkBoxPopup);
+            this.groupBoxBgInfoOptions.Location = new System.Drawing.Point(12, 76);
+            this.groupBoxBgInfoOptions.Name = "groupBoxBgInfoOptions";
+            this.groupBoxBgInfoOptions.Size = new System.Drawing.Size(467, 223);
+            this.groupBoxBgInfoOptions.TabIndex = 15;
+            this.groupBoxBgInfoOptions.TabStop = false;
+            this.groupBoxBgInfoOptions.Text = "BgInfo Options";
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(499, 24);
+            this.menuStrip1.TabIndex = 17;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.openToolStripMenuItem,
+            this.saveToolStripMenuItem,
+            this.exitToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.S)));
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(122, 20);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // openToolStripMenuItem
+            // 
+            this.openToolStripMenuItem.Name = "openToolStripMenuItem";
+            this.openToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.openToolStripMenuItem.Text = "Open";
+            this.openToolStripMenuItem.Click += new System.EventHandler(this.openToolStripMenuItem_Click);
+            // 
+            // saveToolStripMenuItem
+            // 
+            this.saveToolStripMenuItem.Name = "saveToolStripMenuItem";
+            this.saveToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.S)));
+            this.saveToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.saveToolStripMenuItem.Text = "Save";
+            this.saveToolStripMenuItem.Click += new System.EventHandler(this.saveToolStripMenuItem_Click);
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.exitToolStripMenuItem.Text = "Exit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(665, 304);
+            this.ClientSize = new System.Drawing.Size(499, 310);
+            this.Controls.Add(this.groupBoxBgInfoOptions);
+            this.Controls.Add(this.btnSetBgInfoPath);
+            this.Controls.Add(this.txtBgInfoPath);
+            this.Controls.Add(this.lblBgInfoPath);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.MaximizeBox = false;
             this.Name = "Form1";
             this.Text = "BgAssist Config";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownTimer)).EndInit();
+            this.groupBoxBgInfoOptions.ResumeLayout(false);
+            this.groupBoxBgInfoOptions.PerformLayout();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Label lblTimer;
+        private System.Windows.Forms.NumericUpDown numericUpDownTimer;
+        private System.Windows.Forms.Label lblPath;
+        private System.Windows.Forms.Button btnSetConfigPath;
+        private System.Windows.Forms.TextBox txtBgInfoConfigPath;
+        private System.Windows.Forms.Label lblBgInfoPath;
+        private System.Windows.Forms.TextBox txtBgInfoPath;
+        private System.Windows.Forms.Button btnSetBgInfoPath;
+        private System.Windows.Forms.CheckBox checkBoxPopup;
+        private System.Windows.Forms.CheckBox checkBoxSilent;
+        private System.Windows.Forms.CheckBox checkBoxTaskbar;
+        private System.Windows.Forms.CheckBox checkBoxAll;
+        private System.Windows.Forms.CheckBox checkBoxLog;
+        private System.Windows.Forms.CheckBox checkBoxRTF;
+        private System.Windows.Forms.GroupBox groupBoxBgInfoOptions;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem openToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem saveToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
     }
 }
 
--- BgAssist-Config/Form1.cs
+++ BgAssist-Config/Form1.cs
@@ -1,12 +1,15 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel;
+using System.Configuration;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Windows.Forms.VisualStyles;
 
 namespace BgAssist_Config
 {
@@ -15,6 +18,120 @@ namespace BgAssist_Config
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            loadAppConfig();
+        }
+
+        private void btnSetConfigPath_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialogConfigPath = new OpenFileDialog
+            {
+                Title = "Set BgInfo Configuration File Path",
+                CheckFileExists = true,
+                CheckPathExists = true,
+                Filter = "BgInfo Configuration Files (*.big)|"
+            };
+
+            openFileDialogConfigPath.FileOk += delegate (object s, CancelEventArgs ev)
+            {
+                string filename = openFileDialogConfigPath.SafeFileName;
+                string extension = Path.GetExtension(filename).ToUpper();
+
+                if (extension != ".BGI")
+                {
+                    MessageBox.Show("Must choose *.bgi");
+                    ev.Cancel = true;
+                }
+            };
+
+            if (openFileDialogConfigPath.ShowDialog() == DialogResult.OK)
+            {
+                txtBgInfoConfigPath.Text = openFileDialogConfigPath.FileName;
+            }
+        }
+
+        private void btnSetBgInfoPath_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialogBgInfo = new OpenFileDialog
+            {
+                Title = "Set BgInfo64 Path",
+                CheckFileExists = true,
+                CheckPathExists = true,
+                Filter = "BgInfo64 (Bginfo64.exe)|"
+            };
+
+            openFileDialogBgInfo.FileOk += delegate (object s, CancelEventArgs ev)
+            {
+                if (openFileDialogBgInfo.SafeFileName != "Bginfo64.exe")
+                {
+                    MessageBox.Show("Must choose Bginfo64.exe");
+                    ev.Cancel = true;
+                }
+            };
+
+            if (openFileDialogBgInfo.ShowDialog() == DialogResult.OK)
+            {
+                txtBgInfoPath.Text = openFileDialogBgInfo.FileName;
+            }
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void openToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            loadAppConfig();
+        }
+
+        private void loadAppConfig()
+        {
+            txtBgInfoPath.Text = ConfigurationManager.AppSettings["BgInfoPath"];
+            txtBgInfoConfigPath.Text = ConfigurationManager.AppSettings["BgInfoConfigPath"];
+            numericUpDownTimer.Value = Convert.ToDecimal(ConfigurationManager.AppSettings["BgInfoTimer"]);
+            checkBoxPopup.Checked = Convert.ToBoolean(ConfigurationManager.AppSettings["BgInfoPopup"]);
+            checkBoxSilent.Checked = Convert.ToBoolean(ConfigurationManager.AppSettings["BgInfoSilent"]);
+            checkBoxTaskbar.Checked = Convert.ToBoolean(ConfigurationManager.AppSettings["BgInfoTaskbar"]);
+            checkBoxAll.Checked = Convert.ToBoolean(ConfigurationManager.AppSettings["BgInfoAll"]);
+            checkBoxLog.Checked = Convert.ToBoolean(ConfigurationManager.AppSettings["BgInfoLog"]);
+            checkBoxRTF.Checked = Convert.ToBoolean(ConfigurationManager.AppSettings["BgInfoRTF"]);
+        }
+
+        private void saveToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Configuration config = ConfigurationManager.OpenExeConfiguration(Application.ExecutablePath);
+            config.AppSettings.Settings.Remove("BgInfoPath");
+            config.AppSettings.Settings.Add("BgInfoPath", txtBgInfoPath.Text);
+
+            config.AppSettings.Settings.Remove("BgInfoConfigPath");
+            config.AppSettings.Settings.Add("BgInfoConfigPath", txtBgInfoConfigPath.Text);
+
+            config.AppSettings.Settings.Remove("BgInfoTimer");
+            config.AppSettings.Settings.Add("BgInfoTimer", numericUpDownTimer.Value.ToString());
+
+            config.AppSettings.Settings.Remove("BgInfoPopup");
+            config.AppSettings.Settings.Add("BgInfoPopup", checkBoxPopup.Checked.ToString());
+
+            config.AppSettings.Settings.Remove("BgInfoSilent");
+            config.AppSettings.Settings.Add("BgInfoSilent", checkBoxSilent.Checked.ToString());
+
+            config.AppSettings.Settings.Remove("BgInfoTaskbar");
+            config.AppSettings.Settings.Add("BgInfoTaskbar", checkBoxTaskbar.Checked.ToString());
+
+            config.AppSettings.Settings.Remove("BgInfoAll");
+            config.AppSettings.Settings.Add("BgInfoAll", checkBoxAll.Checked.ToString());
+
+            config.AppSettings.Settings.Remove("BgInfoLog");
+            config.AppSettings.Settings.Add("BgInfoLog", checkBoxLog.Checked.ToString());
+
+            config.AppSettings.Settings.Remove("BgInfoRTF");
+            config.AppSettings.Settings.Add("BgInfoRTF", checkBoxRTF.Checked.ToString());
+
+            config.Save(ConfigurationSaveMode.Minimal);
         }
     }
 }
